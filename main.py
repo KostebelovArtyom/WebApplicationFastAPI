b7b350f59b798a36a6e8c78eef73b43e9ea52c82
KurodaChinetsu <kostebelovartem@yandex.ru>
2024-09-12 04:32:48 +0300

    comments

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 import uvicorn
 from fastapi import FastAPI
 
-from views.api import api_router
+from views.api import api_router  # Объявил новое приложение FasrAPI
 
 app = FastAPI()
-app.include_router(api_router)
+app.include_router(api_router)  # Подключил api.router к основному app
 
 
 if __name__ == "__main__":
